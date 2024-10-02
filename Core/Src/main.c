a70b7773d1d4fd4675023b0d1ff5c7ba067c6749
Iskled <iskled.adebayo@gmail.com>
2024-10-02 09:34:28 +0100

    Worked on the comment sections

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,7 +4,6 @@
   * @file           : main.c
   * @brief          : Main program for Interrupt controlled Motors
   * Bare repository created: 01/10/2024
-  * git 2nd commit 01/10/2024
   ******************************************************************************
   * @attention
   *
